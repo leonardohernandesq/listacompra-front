6f0a13c4e91bdb6bd3b4468f3a9de1f845527404
Leonardo Hernandes <62108109+leonardohernandesq@users.noreply.github.com>
2025-04-03 09:42:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <StoreProvider >
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-900`}
         >
           {children}
         </body>
