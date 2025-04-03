461bbfbc8357a76d7f0ca3ba554d372a5fbd9816
Leonardo Hernandes <62108109+leonardohernandesq@users.noreply.github.com>
2025-04-03 09:39:33 +0100

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react'
 
 export const Container = ({children}: PropsWithChildren) => {
   return (
-    <div className='w-full max-w-7xl mx-auto relative'>
+    <div className='w-full max-w-7xl mx-auto relative bg-zinc-900'>
         {children}
     </div>
   )
